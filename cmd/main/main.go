0bcd8a7477687d93ba8d959f8cedc1a58c5c92b9
ZenoHwanEth <hwan2221@hotmail.com>
2023-07-19 11:11:58 +0800

    adding main.go

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/ZenoHwanEth/book-management-system-using-go-and-mysql/pkg/routes"
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:9010", r))
+}
